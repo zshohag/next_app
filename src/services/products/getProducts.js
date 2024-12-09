bebe2187cf52910c342da5c4817894c741c677d3
zshohag <zds.mee@gmail.com>
2024-12-09 18:27:42 +0600

    newadd

--- src/services/products/getProducts.js
+++ src/services/products/getProducts.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProducts = async () => {
   try {
     const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/services/api/get-all`
+      `${process.env.NEXT_PUBLIC_API_URL}/services/api/get-all`
     );
     return res.data;
   } catch (error) {
