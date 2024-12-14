a56cc722ac6cf8a99bde9dc981e119037a828fc5
zshohag <zds.mee@gmail.com>
2024-12-14 18:54:03 +0600

    z

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: false, // Ensure this is set to 'false'
   theme: {
     extend: {
       colors: {
